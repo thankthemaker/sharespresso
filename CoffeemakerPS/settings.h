a5f1f63b2553b2a95e4168b249395bdf0ba9c8d0
dgey <tonymande@gmx.de>
2017-12-19 08:05:09 +0100

    no comment

---
+++ CoffeemakerPS/settings.h
@@ -0,0 +1,15 @@
+// Your WiFi credentials.
+// Set password to "" for open networks.
+#define SETTINGS_H_
+
+#define MAX_CARDS           40  // number of cards, max is (1024-11*2)/6=167 on Arduino Uno
+
+#define WIFI_SSID           ""
+#define WIFI_PASSWORD       ""
+
+#define MQTT_SERVER         "mqtt.thank-the-maker.org"
+#define MQTT_PORT           1833
+#define MQTT_TOPIC_IN       "coffeemaker/gigax8/in"
+#define MQTT_TOPIC_OUT      "coffeemaker/gigax8/out"
+#define MQTT_USERNAME       ""
+#define MQTT_PASSWORD       ""
