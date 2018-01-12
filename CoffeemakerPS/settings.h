6c9a458ec7697a4f00da4d95439af650e5f69b04
dgey <tonymande@gmx.de>
2018-01-12 20:34:39 +0100

    Updated settings

--- CoffeemakerPS/settings.h
+++ CoffeemakerPS/settings.h
@@ -1,7 +1,6 @@
 #define SETTINGS_H_
 
-// number of cards, max is (1024-11*2)/6=167 on Arduino Uno
-#define MAX_CARDS           40  
+#define OTA_UPDATE_URL      "http://www.thank-the-maker.org/ota/firmware.bin"
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
@@ -9,23 +8,60 @@
 #define WIFI_PASSWORD       "<YOUR_WIFI_PASSWORD>"
 
 // MQTT settings
-#define MQTT_SERVER         "<YOUR_MQTT_SERVER>"
+#define MQTT_SERVER         "mqtt.thank-the-maker.org"
 #define MQTT_PORT           1883
-#define MQTT_TOPIC_IN       "coffeemaker/gigax8/in"
-#define MQTT_TOPIC_OUT      "coffeemaker/gigax8/out"
+#define MQTT_TOPIC_IN       "/coffeemaker/gigax8/toCoffeemaker"
+#define MQTT_TOPIC_OUT      "/coffeemaker/gigax8/fromCoffeemaker"
 #define MQTT_USERNAME       "<YOUR_MQTT_USERNAME>"
 #define MQTT_PASSWORD       "<YOUR_MQTT_PASSWORD>"  
 
-// Default PIN definition
-#define BUZPIN              0
-#define OLED_SDA_PIN        4
-#define OLED_SDC_PIN        5
-#define BLE_RX_PIN          7
-#define BLE_TX_PIN          8
-#define JURA_RX_PIN         15
-#define JURA_TX_PIN         16 
+#define NODEMCU_1_0
+//#define SPARKFUN
 
-#define PN532_SS            (2)
-#define PN532_SCK           (14)
-#define PN532_MISO          (12)
-#define PN532_MOSI          (13)
+// PIN definition Sparkfun ESP8266 Thing Dev
+#ifdef SPARKFUN
+ #define EEPROM_SIZE_BYTES   512
+ // number of cards, max is (512-11*2)/6=81 on ESP8266 Sparkfun Thing Dev
+ #define MAX_CARDS           40  // only 60 cards to keep some space reserved  
+ // offset for pricelist 512-11*2=490 on ESP8266 Sparkfun Thing Dev
+ #define PRICELIST_ADDRESS_OFFSET     489
+
+ #define BUZPIN              0
+ #define OLED_SDA_PIN        4
+ #define OLED_SDC_PIN        5
+ #define BLE_RX_PIN          7
+ #define BLE_TX_PIN          8
+ #define JURA_RX_PIN         15
+ #define JURA_TX_PIN         16 
+
+ #define PN532_SS            (2)
+ #define PN532_SCK           (14)
+ #define PN532_MISO          (12)
+ #define PN532_MOSI          (13)
+
+ #define BLE_ENABLED         1
+#endif
+
+// PIN definition AI Thinker NodeMCU 1.0
+#ifdef NODEMCU_1_0
+  #define EEPROM_SIZE_BYTES   4096
+  // number of cards, max is (4096-11*2)/6=679 on NodeMCU v1.0
+  #define MAX_CARDS           40  // only 40 cards to keep some space reserved  
+  // offset for pricelist 512-11*2=490 on ESP8266 Sparkfun Thing Dev
+  #define PRICELIST_ADDRESS_OFFSET     4073
+
+  #define BUZPIN              0  //D3
+  #define OLED_SDA_PIN        4  //D2
+  #define OLED_SDC_PIN        5  //D1
+  #define BLE_RX_PIN          3  //RX
+  #define BLE_TX_PIN          1  //TX
+  #define JURA_RX_PIN         15
+  #define JURA_TX_PIN         16 
+
+ #define PN532_SCK            (14) //D5
+ #define PN532_MISO           (12) //D6
+ #define PN532_MOSI           (13) //D7
+ #define PN532_SS             (15) //D8
+
+ #define BLE_ENABLED         0
+#endif
