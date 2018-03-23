4016ca3d06182b392574e41caf8b1fe264cc1e2e
dgey <tonymande@gmx.de>
2018-03-23 07:28:00 +0100

    Optimized products

---
+++ CoffeemakerPS/journal.cpp
@@ -0,0 +1,40 @@
+#include "journal.h"
+
+void Journal::initJournal() {
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SD_CARD_CS)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+void Journal::writeJournal(String timestamp, String cardId, String product, String price) {
+
+  String data = timestamp + ";" + cardId + ";" + product + ";" + price;
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("journal.csv", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(data);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(data);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening journal.csv");
+  }
+}
+
+
+
+
+
+
--- CoffeemakerPS/jurax7.h
+++ CoffeemakerPS/jurax7.h
@@ -5,10 +5,10 @@
 
 class JuraX7 : public ICoffeeMaker {
   public:
-    // https://stackoverflow.com/questions/35413821/how-to-fix-this-array-used-as-initializer-error
-    // product codes send by coffeemakers "?PA<x>\r\n", just <x>
-    char products[11] = {'A', 'B', 'C', 'H', 'D', 'E', 'K', 'J', 'F', 'G', '\n'};
+    // product codes send by coffeemakers "?PA<x>\r\n", just <x>    
+    //std::map <char, String> products;
     JuraX7(IDisplay *oled, Buzzer *buzzer);
-    char* getProducts();
+    //std::map <char, String> getProducts();
+    //int getIndexForProduct(char productCode);
 };
 #endif
